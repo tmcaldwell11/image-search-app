efed52e2451149cb217e2dfa6585ebc09359458c
tmcaldwell11 <tmcaldwell11@gmail.com>
2023-02-22 21:45:47 -0500

    update search useState

--- src/imageSearchForm.js
+++ src/imageSearchForm.js
@@ -1,6 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function imageSearchForm() {
+  //create hook for useState
+  const [query, setQuery] = useState("");
+  console.log(query);
+  
   return (
     <>
     <form className="form"> 
@@ -11,6 +15,8 @@ export default function imageSearchForm() {
           name="query"
           className="input"
           placeholder={'Search images here...'}
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
         />
         <button type="submit" className="button">
           Search
